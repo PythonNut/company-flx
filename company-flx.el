dcd59abce8a0815c992178aa3dcd8f7caa67022c
PythonNut <PythonNut@users.noreply.github.com>
2015-10-17 03:56:33 +0000

    Add initial core functions

---
+++ company-flx.el
@@ -0,0 +1,176 @@
+;; -*- lexical-binding: t -*-
+
+(eval-when-compile
+  (with-demoted-errors "Load error: %s"
+    (require 'cl-lib)
+    (require 'company)
+    (require 'flx)))
+
+(defvar company-flx-cache)
+(defvar company-flx-limit 500)
+
+(with-no-warnings
+  (defun company-flx-commonality (strs)
+    (cl-letf* ((commonality-cache (make-hash-table :test 'equal :size 200))
+               ((symbol-function
+                 #'fuzzy-commonality)
+                (lambda (strs)
+                  (let ((hash-value (gethash strs commonality-cache nil)))
+                    (if hash-value
+                        (if (eq hash-value 'nothing)
+                            nil
+                          hash-value)
+
+                      (setq strs (mapcar #'string-to-list strs))
+                      (let ((res) (tried) (idx))
+                        (dolist (char (car strs))
+                          (unless (memq char tried)
+                            (catch 'notfound
+                              (setq idx (mapcar (lambda (str)
+                                                  (or
+                                                   (cl-position char str)
+                                                   (throw 'notfound nil)))
+                                                strs))
+                              (push (cons char
+                                          (fuzzy-commonality
+                                           (cl-mapcar (lambda (str idx)
+                                                        (cl-subseq str (1+ idx)))
+                                                      strs idx)))
+                                    res)
+                              (push char tried))))
+                        (setq res (if res
+                                      (cl-reduce
+                                       (lambda (a b)
+                                         (if (> (length a) (length b)) a b))
+                                       res)
+                                    nil))
+                        (puthash strs
+                                 (if res res 'nothing)
+                                 commonality-cache)
+                        res))))))
+      (concat (fuzzy-commonality strs)))))
+
+(defun company-flx-find-holes (merged str)
+  (let ((holes) (matches (cdr (flx-score str merged company-flx-cache))))
+    (dolist (i (number-sequence 0 (- (length matches) 2)))
+      (when (>
+             (elt matches (1+ i))
+             (1+ (elt matches i)))
+        (push (1+ i) holes)))
+    (unless (<= (length str) (car (last matches)))
+      (push (length merged) holes))
+    holes))
+
+(defun company-flx-merge (strs)
+  (let ((common (company-flx-commonality strs))
+        (holes))
+    (setq holes (make-vector (1+ (length common)) 0))
+    (dolist (str strs)
+      (dolist (hole (company-flx-find-holes common str))
+        (cl-incf (elt holes hole))))
+
+    (cons common (append holes nil))))
+
+(defun company-flx-completion (string table predicate point
+                                      &optional all-p)
+  (let* ((beforepoint (substring string 0 point))
+         (afterpoint (substring string point))
+         (boundaries (completion-boundaries beforepoint table predicate afterpoint))
+         (prefix (substring beforepoint 0 (car boundaries)))
+         (infix (concat
+                 (substring beforepoint (car boundaries))
+                 (substring afterpoint 0 (cdr boundaries))))
+         (suffix (substring afterpoint (cdr boundaries)))
+         ;; |-              string                  -|
+         ;;              point^
+         ;;            |-  boundaries -|
+         ;; |- prefix -|-    infix    -|-  suffix   -|
+         ;;
+         ;; Infix is the part supposed to be completed by table, AFAIKT.
+         (regexp (concat "\\`"
+                         (mapconcat
+                          (lambda (x)
+                            (setq x (string x))
+                            (concat "[^" x "]*" (regexp-quote x)))
+                          infix
+                          "")))
+         (completion-regexp-list (cons regexp completion-regexp-list))
+         (candidates (all-completions prefix table predicate)))
+
+    (if all-p
+        ;; Implement completion-all-completions interface
+        (when candidates
+          ;; Not doing this may result in an error.
+          (setcdr (last candidates) (length prefix))
+          candidates)
+      ;; Implement completion-try-completions interface
+      (if (= (length candidates) 1)
+          (if (equal infix (car candidates))
+              t
+            ;; Avoid quirk of double / for filename completion. I don't
+            ;; know how this is *supposed* to be handled.
+            (when (and (> (length (car candidates)) 0)
+                       (> (length suffix) 0)
+                       (char-equal (aref (car candidates)
+                                         (1- (length (car candidates))))
+                                   (aref suffix 0)))
+              (setq suffix (substring suffix 1)))
+            (cons (concat prefix (car candidates) suffix)
+                  (length (concat prefix (car candidates)))))
+        (if (= (length infix) 0)
+            (cons string point)
+          (cl-destructuring-bind (merged . holes)
+              (company-flx-merge candidates)
+            (cons
+             (concat prefix merged suffix)
+             (+ (length prefix)
+                (cl-position (apply #'max holes) holes)))))))))
+
+(defun company-flx-try-completion (string table predicate point)
+  (company-flx-completion string table predicate point))
+(defun company-flx-all-completions (string table predicate point)
+  (company-flx-completion string table predicate point 'all))
+
+(add-to-list 'completion-styles-alist
+             '(fuzzy
+               company-flx-try-completion
+               company-flx-all-completions
+               "An intelligent fuzzy matching completion style."))
+
+(defun company-flx-company-capf-advice (old-fun &rest args)
+  (let ((completion-styles (list 'fuzzy)))
+    (apply old-fun args)))
+
+(defun company-flx-transformer (cands)
+  (require 'flx)
+  (or company-flx-cache
+      (setq company-flx-cache (flx-make-string-cache 'flx-get-heatmap-str)))
+
+  (let ((num-cands (length cands)))
+    (mapcar #'car
+            (sort (mapcar
+                   (lambda (cand)
+                     (cons cand
+                           (or (car (flx-score cand
+                                               company-prefix
+                                               company-flx-cache))
+                               0)))
+                   (if (< num-cands company-flx-limit)
+                       cands
+                     (let ((seq (sort cands (lambda (c1 c2)
+                                              (< (length c1)
+                                                 (length c2)))))
+                           (end (min company-flx-limit
+                                     num-cands))
+                           (result nil))
+                       (while (and seq
+                                   (>= (setq end (1- end)) 0))
+                         (push (pop seq) result))
+                       result)))
+                  (lambda (c1 c2)
+                    (> (cdr c1)
+                       (cdr c2)))))))
+
+(setq company-transformers (list #'company-flx-transformer))
+
+(provide 'config-company)
